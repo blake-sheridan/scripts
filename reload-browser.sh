09c8427d1b56ee28d3735a7958b3378df359abf2
Blake Sheridan <blake.m.sheridan@gmail.com>
2016-08-17 08:21:04 -0400

    xte for ctrl+r

--- reload-browser.sh
+++ reload-browser.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 focused_id=$(xdotool getwindowfocus)
-xdotool search --onlyvisible --class $1 windowfocus key 'ctrl+r'
+xdotool search --onlyvisible --class $1 windowactivate --sync
+xte "keydown Control_L" "key r" "keyup Control_L"
 xdotool windowactivate --sync $focused_id
