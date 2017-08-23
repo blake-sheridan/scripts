3ef0d5063f359ed157a7960f5142df5460d7b2b4
Blake Sheridan <blake.m.sheridan@gmail.com>
2017-08-23 10:50:43 -0400

    Make focus/select work per-viewport
    Implement these in Python as the logic gets more complicated.

--- focus-window.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-read id < "$XDG_RUNTIME_DIR/ui/$1.id"
-
-xdotool windowactivate $id
--- select-window.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir -p $XDG_RUNTIME_DIR/ui
-
-xdotool selectwindow > $XDG_RUNTIME_DIR/ui/$1.id
-
